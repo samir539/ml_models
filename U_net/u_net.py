3048da5c2cfd86f6f9aa0a884b9bd9d3012dd66e
samir <sc9018@ic.ac.uk>
2023-06-29 13:42:52 +0100

    added contracting path

--- U_net/u_net.py
+++ U_net/u_net.py
@@ -7,9 +7,9 @@ def double_convolution(in_channel,out_channel):
     """
     conv = nn.Sequential(nn.Conv2d(in_channel,out_channel, kernel_size=3),
                          nn.ReLU(inplace=True),
-                         nn.Conv2d(in_channel,out_channel, kernel_size=3),
+                         nn.Conv2d(out_channel,out_channel, kernel_size=3),
                          nn.ReLU(inplace=True))
-     
+    return conv     
 
 class Unet(nn.Module):
     """
@@ -17,7 +17,7 @@ class Unet(nn.Module):
     """
     def __init__(self):
         super().__init__()
-        self.max_pool_2by2 = nn.MaxPool(kernel_size=2,stride=2)
+        self.max_pool_2by2 = nn.MaxPool2d(kernel_size=2,stride=2)
         self.down_conv_1 = double_convolution(1,64)
         self.down_conv_2 = double_convolution(64,128)
         self.down_conv_3 = double_convolution(128,256)
@@ -28,5 +28,21 @@ class Unet(nn.Module):
         """
         take an image and run the forward pass
         """
-        
+        x1 = self.down_conv_1(image) 
+        print(x1.size())
+        x2 = self.max_pool_2by2(x1)
+        x3 = self.down_conv_2(x2) 
+        x4 = self.max_pool_2by2(x3)
+        x5 = self.down_conv_3(x4) 
+        x6 = self.max_pool_2by2(x5)
+        x7 = self.down_conv_4(x6) 
+        x8 = self.max_pool_2by2(x7)
+        x9 = self.down_conv_5(x8) 
+        print(x9.size())
+   
+      
+if __name__ == "__main__":
+    image = torch.rand((1,1,572,572))
+    model = Unet()
+    print(model.forward(image))
 
