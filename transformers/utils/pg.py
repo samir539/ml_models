5100184af46e908c04fdd0c87acce122b7d7fbfc
samir <sc9018@ic.ac.uk>
2023-10-10 13:45:47 +0100

    written as lambda function

--- transformers/utils/pg.py
+++ transformers/utils/pg.py
@@ -13,10 +13,11 @@ char_to_int = lambda char: char_list.index(char) #given a char we get an integer
 int_to_char = lambda int: char_list[int] #given an integer we get a char
 
 #extend to multiple characters
-string_input = "hello"
-string_to_ints = [char_to_int(x) for x in string_input]
+string_to_ints = lambda string_input:[char_to_int(x) for x in string_input]
+ints_to_string = lambda int_input: [int_to_char(x) for x in int_input]
 
-int_input = [46,43,50,50,53]
-ints_to_string = [int_to_char(x) for x in int_input]
+print(string_to_ints("hello world"))
+print(ints_to_string([46, 43, 50, 50, 53, 1, 61, 53, 56, 50, 42]))
 
 # lambda string: for i in string 
+
