a9e71e8c127358da8f0c51921d3c28d9a0502887
samir <sc9018@ic.ac.uk>
2023-10-20 10:01:15 +0100

    basic bigram

--- bigram_model/bigram.py
+++ bigram_model/bigram.py
@@ -24,7 +24,7 @@ def generate_bigram(names_data):
     """
     bigram_dict = {}
     for i in names_data:
-        characters = ["<S>"] + list(i) + ["<E>"]
+        characters = ["."] + list(i) + ["."]
         for j,p in zip(characters,characters[1:]):
             bigram = (j,p)
             bigram_dict[bigram] = bigram_dict.get(bigram,0) + 1
@@ -40,13 +40,14 @@ def generate_bigram_tensor(bigram_dict, visualise=False):
 
     """
     #converisons
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
-        'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','<S>','<E>']
+    letters = ['.','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+        'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     char_to_int = lambda char: letters.index(char) #given a char we get an integer
     int_to_char = lambda int: letters[int] 
     tuple_to_index = lambda tuple: [char_to_int(i) for i in tuple ] #convert tuple to index 
+    
     #init bigram_tensor
-    bigram_tensor = torch.zeros((28,28), dtype=torch.int32)
+    bigram_tensor = torch.zeros((27,27), dtype=torch.int32)
     freq = list(sorted(bigram_dict.items(), key=lambda kv: -kv[1]))
     for i in freq:
         indx = tuple_to_index(i[0])
@@ -56,10 +57,10 @@ def generate_bigram_tensor(bigram_dict, visualise=False):
     if visualise == True:
         plt.figsize=(64,64)
         plt.imshow(bigram_tensor, cmap="magma")
-        for i in range(28):
-            for j in range(28):
+        for i in range(27):
+            for j in range(27):
                 bigramstr =  int_to_char(i) + int_to_char(j)
-                plt.text(j,i, bigramstr, ha="center", va="bottom", color="black",fontsize=5)
+                plt.text(j,i, bigramstr, ha="center", va="bottom", color="grey",fontsize=5)
                 plt.text(j,i,bigram_tensor[i,j].item(), ha="center", va="top", color="grey",fontsize=5)
         plt.axis('off')
         plt.show()
@@ -68,17 +69,63 @@ def generate_bigram_tensor(bigram_dict, visualise=False):
 
 
 b = generate_bigram(names_data)
-out_tensor = generate_bigram_tensor(b,True)
+out_tensor = generate_bigram_tensor(b,False)
+
+##multimodal distribution with broadcasting
 
 
-        
+## generate matrix of probablities
+## sample from the probabilities
+## write a loop to generate bigram names 
+
+def prob_mat(bigram_tensor):
+    """
+    Function to generate a corresponding tensor containing the  probablilties for each bigram 
+    :param bigram_tensor: the frequency tensor of each possible bigram  
+    :return bigram_prob_tensor: the bigram probability tensor 
+    """
+    # bigram_tensor = bigram_tensor.float()
+    sumation_col = bigram_tensor.sum(1,keepdim=True)
+    bigram_prob_tensor = bigram_tensor/sumation_col
+    return bigram_prob_tensor
+
+# print(prob_mat(out_tensor))
+
+
+
+def generate_names(bigram_prob_tensor, num_names):
+    """
+    Function to generate novel names given a probability tensor
+    :param bigram_prob_tensor: the bigram probablity tensor
+    :param num_names: the numbers of names to generate
+    """
+    #converisons
+    letters = ['.','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+        'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    char_to_int = lambda char: letters.index(char) #given a char we get an integer
+    int_to_char = lambda int: letters[int]
+    gen = torch.Generator().manual_seed(77)
+    for i in range(num_names):
+        gen_name = []
+        letter_val  = 0
+        while True:
+            char = int_to_char(letter_val)
+            gen_name.append(char)
+            letter_val =  torch.multinomial(bigram_prob_tensor[letter_val,:],1,replacement=True, generator=gen).item()
+            if letter_val == 0:
+                break
+        print(''.join(gen_name))
+    
+
+prob_mat_out = prob_mat(out_tensor)
+generate_names(prob_mat_out,50)
 
 
 
 
-
-# b = generate_bigram(names_data[:4])
-# print(b)
-# print(generate_bigram_tensor(b))
-
-
+    #for num names
+    # inner loop
+    # pick next letter 
+    # if end character stop 
+    # print out name
+# torch.Generator
