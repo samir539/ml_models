d26c490279202fcc782e712b4054a7f7a0687e3f
samir <sc9018@ic.ac.uk>
2023-10-21 09:50:38 +0100

    fixed training

--- bigram_model/bigram_nn.py
+++ bigram_model/bigram_nn.py
@@ -11,8 +11,8 @@ class bigram_net(nn.Module):
         __init__: Initialise an instance of the object
 
     """
-    def __init__(self, depth,alphabet_len=27):
-        super.__init__()
+    def __init__(self,alphabet_len=27):
+        super().__init__()
         self.layer = nn.Linear(alphabet_len,alphabet_len)
 
     def forward(self,input):
@@ -26,6 +26,7 @@ class bigram_net(nn.Module):
         out = self.layer(input)
         sftmax = nn.Softmax(dim=1) #softmax
         out = sftmax(out)
+        # print("this is ",out.sum(dim=0))
         return out
     
 
@@ -38,7 +39,7 @@ def get_data(names_data):
     :return test_data: pyTorch tensor with one hot enoded values for the test instances
     """
     L1, L2 = [],[]
-    for n in names_data[:2]:
+    for n in names_data:
         characters = ["."] + list(n) + ["."]
         for i,j in zip(characters,characters[1:]):
             L1.append(char_to_int[i])
@@ -46,8 +47,8 @@ def get_data(names_data):
     L1,L2 = torch.tensor(L1), torch.tensor(L2)
 
     #one hot encode 
-    train_data = F.one_hot(L1,num_classes=27)
-    test_data = F.one_hot(L2, num_classes=27)
+    train_data = F.one_hot(L1,num_classes=27).float()
+    test_data = F.one_hot(L2, num_classes=27).float()
     return L1,L2,train_data,test_data
             
 
@@ -62,26 +63,50 @@ def training(data,L2,net,steps,lr):
     # - Optimise
     # training loop 
     num = L2.nelement()
+    print(num)
     for i in range(steps):
+        net.zero_grad()
         out_probs = net(data)
+        # print("these are out probs", out_probs.sum(dim=1))
         loss = -out_probs[torch.arange(num),L2].log().mean()
         print(loss.item())
 
         loss.backward()
         with torch.no_grad():
             for param in net.parameters():
-                param -= lr * param.grad
+                param += -lr * param.grad
 
         #step 
-    
 
 
 
 
-def make_names():
+
+def make_names(num_of_names,net):
     """
     function to make names (run model in inference)
+    :params num_of_names: the number of names to generate
+    :param net: the trained bigram nn
     """
+    for i in range(num_of_names):
+        name = []
+        start_letter = 0
+        while True:
+            input_data = F.one_hot(torch.tensor(start_letter), num_classes=27).float()
+            print(input_data)
+            print("this is net input data",net(input_data))
+            idx = torch.argmax(net(input_data)).item()
+            if idx == 0:
+                break
+            chr = int_to_char[idx]
+            name.append(chr)
+        print(name)
+
+            
+
+
+
+
 
 
 with open("./names.txt", mode='r', encoding="utf-8") as f:
@@ -97,4 +122,17 @@ characters.insert(0,'.')
 char_to_int = {char_val:idx for (idx,char_val) in enumerate(characters)}
 int_to_char = {idx:char_val for (idx, char_val) in enumerate(characters)}
 
-get_data(names_data)
+L1,L2, train_data, test_data = get_data(names_data)
+
+bigram_nn = bigram_net()
+
+training(train_data,L2,bigram_nn,10,lr=10)
+
+make_names(10,bigram_net)
+
+
+
+
+
+
+
